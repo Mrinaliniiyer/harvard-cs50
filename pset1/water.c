5570f1b9d311a33e92efbe51265afb963008980b
alanthony333 <alanthony333@gmail.com>
2017-08-23 04:12:13 +0800

    Added comments to understand code

--- pset1/water.c
+++ pset1/water.c
@@ -2,7 +2,12 @@
 #include <stdio.h>
 
 int main(void) {
+	
+	//provide user input and convert to integer
     printf("Minutes: ");
     int minutes = get_int();
+	
+	
+	//convert minutes to bottles by multiplying the minutes by 12 then printing it
     printf("Bottles: %i\n", minutes * 12);
 }
